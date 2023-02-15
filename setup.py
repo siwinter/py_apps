42b4b8371787a553a307c72cdc0495950eb3838a
siwinter <45730097+siwinter@users.noreply.github.com>
2023-02-15 01:12:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    name='rainradar',  
+    name='py_apps',  
     version='0.0.1',
     author="siwinter",
 #    author_email="andrew@ao.gl",
     description="A virtual rainsensor",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/siwinter/rainradar",
-    packages=["rainradar"],
+    url="https://github.com/siwinter/py_apps",
+    packages=["py_apps"],
     entry_points = {
         "console_scripts": ['rainradar = py_apps.rainradar:main',
         'vpnController = py_apps.vpnControll:main',
